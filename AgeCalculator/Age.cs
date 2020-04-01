a9cf8541201d722136834a683d04899c49b594fa
Khanyisile <khanyisilemathe@chillisoft.co.za>
2020-04-01 09:15:08 +0200

    added project

---
+++ AgeCalculator/Age.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    public class Age
+    {
+        public static int CalculateAge(DateTime dob, DateTime currentDate)
+
+        {
+            var age = 0;
+
+            var dobYear = dob.Year;
+            var dobMonth = dob.Month;
+            var dobDay = dob.Day;
+
+            var currentDateYear = currentDate.Year;
+            var currentDateMonth = currentDate.Month;
+            var currentDateDay = currentDate.Day;
+
+            for (int i = dobYear; i < currentDateYear; i++)
+            {
+                if (dobYear < currentDateYear)
+                {
+                    age++;
+                }
+            }
+
+            if (dobMonth < currentDateMonth)
+            {
+                age = age - 1;
+            }
+
+            if (dobMonth == currentDateMonth)
+            {
+                if (dobDay < currentDateDay)
+                {
+                    age = age - 1;
+                }
+            }
+
+            return age;
+        }
+    }
+}
---
+++ TestAgeCalculator/TestAge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AgeCalculator;
+using NUnit.Framework;
+
+namespace TestAgeCalculator
+{
+    [TestFixture]
+    public class TestAge
+    {
+
+        [TestCase(29, "1990-02-01")]
+        [TestCase(29, "1990-03-09")]
+        [TestCase(30, "1990-04-01")]
+        [TestCase(30, "1990-10-26")]
+
+        [TestCase(28, "1991-02-12")]
+        [TestCase(28, "1991-03-08")]
+        [TestCase(29, "1991-04-01")]
+        [TestCase(29, "1991-11-11")]
+
+        [TestCase(33, "1986-01-03")]
+        [TestCase(33, "1986-02-03")]
+        [TestCase(34, "1986-04-01")]
+        [TestCase(34, "1986-12-03")]
+
+
+        public void CountAge(int age, DateTime dob)
+        {
+            var currentDate = DateTime.Now;
+
+            Assert.AreEqual(age, Age.CalculateAge(dob, currentDate));
+        }
+    }
+}
